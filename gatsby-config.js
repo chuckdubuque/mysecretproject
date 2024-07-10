5226d07983b2267e678dc1683691c18ce1d196eb
Chuck Dubuque <chuck.dubuque@gmail.com>
2024-07-10 11:30:48 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `commons2`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
